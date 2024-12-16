a882e5b6bc5c9ddd599cf72fa136c1990fc5bc8c
Birccy <rpboado3@student.apc.edu.ph>
2024-12-16 14:54:24 +0800

    added new button to change pfp

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,9 +1,16 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  // This function will change the content of the 'demo' paragraph
+  document.getElementById("demo").innerHTML = "I can change paragraphs at the click of a button!";
+}
 
+function myFunction2() {
+  // This function will retrieve the first paragraph (name) and display it as the website owner's name
+  const element = document.getElementsByTagName("p"); // Get all <p> elements
+  const name = element[0].innerHTML; // The first <p> contains the name: "Ric Brian P. Boado"
+  document.getElementById("demo").innerHTML = 'The owner of this website is... ' + name;
+}
 
-  function myFunction2() {
-    const element = document.getElementsByTagName("p");
-    document.getElementById("demo").innerHTML = 'The name of the website owner!' + element[0].innerHTML;
-  }
+function changeProfilePicture() {
+  // This function will change the profile picture dynamically
+  document.getElementById("profileImage").src = "https://via.placeholder.com/200"; // New image URL
+}
